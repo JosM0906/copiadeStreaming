a8e3b07fcde95bc867d96a4dc1eb14d10db2bef0
JosM0906 <eosorioi1@miumg.edu.gt>
2025-08-05 00:28:50 -0600

    cambios en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
+
+// Categorías
 import Quimica from './pages/Categorias/Quimica';
 import VideoQuimica from './pages/Categorias/videoquimica';
+
+import Historia from './pages/Categorias/Historia';
+import VideoHistoria from './pages/Categorias/videohistoria';
+
+import Matematicas from './pages/Categorias/Matematicas';
+import VideoMatematicas from './pages/Categorias/videomatematicas';
+
+import Biologia from './pages/Categorias/Biologia';
+import VideoBiologia from './pages/Categorias/videobiologia';
+
+import Computacion from './pages/Categorias/Computacion';
+import VideoComputacion from './pages/Categorias/videocomputacion';
+
 
 const App = () => {
   const location = useLocation();
 
   return (
-    <>
+    <Router>
       {/* Oculta la Navbar si estás en la página de Login */}
       {location.pathname !== '/' && <Navbar />}
 
@@ -21,7 +36,27 @@ const App = () => {
         <Route path="/quimica" element={<Quimica />} />
         <Route path="/video-quimica" element={<VideoQuimica />} />
       </Routes>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path='/video-historia' element={<VideoHistoria/>} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/matematicas" element={<Matematicas />} />
+        <Route path='/video-matematicas' element={<VideoMatematicas/>} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/biologia" element={<Biologia />} />
+        <Route path='/video-biologia' element={<VideoBiologia/>} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/computacion" element={<Computacion />} />
+        <Route path='/video-computacion' element={<VideoComputacion/>} />
+      </Routes>
+    </Router>
   );
 };
 
