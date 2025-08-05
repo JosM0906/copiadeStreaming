3bf9ce36570fe1028b56dc318c49342c31f5ad8a
Diana Avendano <digua2021@icloud.com>
2025-08-04 22:09:48 -0600

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
+import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Quimica from './pages/Categorias/Quimica';
 import VideoQuimica from './pages/Categorias/videoquimica';
-import Modal from './pages/Modal/Modal';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <Navbar />
+    <>
+      {/* Oculta la Navbar si estás en la página de Login */}
+      {location.pathname !== '/' && <Navbar />}
+
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/quimica" element={<Quimica />} />
-        <Route path='/video-quimica' element={<VideoQuimica />} />
+        <Route path="/video-quimica" element={<VideoQuimica />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className='Login'>
-      
+      <img src={logo} className='login-logo' alt=""></img>
+      <div className="login-form">
+        <h1>Iniciar Sesión</h1>
+        <form>
+          <input type="text" placeholder='Your name'></input>
+          <input type="email" placeholder='Email'></input>
+          <input type="password" placeholder='Email'></input>
+        </form>
+      </div>
     </div>
   )
 }
