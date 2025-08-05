afbe8a47ea135f0e27e65eeb6c49427e28b2d521
Diana Avendano <digua2021@icloud.com>
2025-08-04 23:38:17 -0600

    Navbar responsiva

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
@@ -8,10 +8,23 @@ import profile_img from '../../assets/profile_img.png';
 import caret_icon from '../../assets/caret_icon.svg';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="logo" />
+
+        {/* Botón para pantallas pequeñas */}
+        <button className="menu-toggle" onClick={toggleMenu}>
+          Explorar
+        </button>
+
+        {/* Menú normal para pantallas grandes */}
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/matematicas">Matemáticas</Link></li>
@@ -20,12 +33,23 @@ const Navbar = () => {
           <li><Link to="/computacion">Computación</Link></li>
           <li><Link to="/quimica">Química</Link></li>
         </ul>
+
+        {/* Menú desplegable en móvil */}
+        <div className={`menu-dropdown ${showMenu ? 'show' : ''}`}>
+          <Link to="/home">Home</Link>
+          <Link to="/matematicas">Matemáticas</Link>
+          <Link to="/historia">Historia</Link>
+          <Link to="/biologia">Biología</Link>
+          <Link to="/computacion">Computación</Link>
+          <Link to="/quimica">Química</Link>
+        </div>
       </div>
+
       <div className="navbar-right">
-        <img src={search_icon} alt="" className='icons'/>
-        <img src={bell_icon} alt="" className='icons'/>
+        <img src={search_icon} alt="" className='icons' />
+        <img src={bell_icon} alt="" className='icons' />
         <div className='navbar-profile'>
-          <img src={profile_img} alt="" className='profile'/>
+          <img src={profile_img} alt="" className='profile' />
           <img src={caret_icon} alt="" />
           <div className='dropdown'>
             <p>Sign out</p>
